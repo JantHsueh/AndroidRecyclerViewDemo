cc9caf7269b6af7acdac0765fc4ac22c6123710d
JantXue <384324069@qq.com>
2017-03-10 19:02:54 +0800

    modified .gitignore and add item xml

--- app/src/main/java/com/frankzhu/recyclerviewdemo/anim/AbstractItemAnimator.java
+++ app/src/main/java/com/frankzhu/recyclerviewdemo/anim/AbstractItemAnimator.java
@@ -1,7 +1,5 @@
 package com.frankzhu.recyclerviewdemo.anim;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 
 /**
@@ -22,24 +20,44 @@ public class AbstractItemAnimator extends RecyclerView.ItemAnimator {
     }
 
     @Override
-    public boolean animateDisappearance(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @Nullable ItemHolderInfo postLayoutInfo) {
+    public boolean animateRemove(RecyclerView.ViewHolder holder) {
         return false;
     }
 
     @Override
-    public boolean animateAppearance(@NonNull RecyclerView.ViewHolder viewHolder, @Nullable ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+    public boolean animateAdd(RecyclerView.ViewHolder holder) {
         return false;
     }
 
     @Override
-    public boolean animatePersistence(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+    public boolean animateMove(RecyclerView.ViewHolder holder, int fromX, int fromY, int toX, int toY) {
         return false;
     }
 
     @Override
-    public boolean animateChange(@NonNull RecyclerView.ViewHolder oldHolder, @NonNull RecyclerView.ViewHolder newHolder, @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+    public boolean animateChange(RecyclerView.ViewHolder oldHolder, RecyclerView.ViewHolder newHolder, int fromLeft, int fromTop, int toLeft, int toTop) {
         return false;
     }
+
+//    @Override
+//    public boolean animateDisappearance(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @Nullable ItemHolderInfo postLayoutInfo) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean animateAppearance(@NonNull RecyclerView.ViewHolder viewHolder, @Nullable ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean animatePersistence(@NonNull RecyclerView.ViewHolder viewHolder, @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+//        return false;
+//    }
+//
+//    @Override
+//    public boolean animateChange(@NonNull RecyclerView.ViewHolder oldHolder, @NonNull RecyclerView.ViewHolder newHolder, @NonNull ItemHolderInfo preLayoutInfo, @NonNull ItemHolderInfo postLayoutInfo) {
+//        return false;
+//    }
 
     @Override
     public void endAnimation(RecyclerView.ViewHolder item) {
